7ed1f84421e36137e30d8f8c52fa7b76d367bc29
RockosBos <32206608+RockosBos@users.noreply.github.com>
2022-03-12 22:37:54 -0500

    Added PathFinder

---
+++ 2022-Competition/src/main/java/frc/robot/commands/Autonomous/AutonomousCommandGroups/AutonomousPathPlannerTestCommandGroup.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Autonomous.AutonomousCommandGroups;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.subsystems.DriveSubsystem;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class AutonomousPathPlannerTestCommandGroup extends SequentialCommandGroup {
+  /** Creates a new AutonomousPathPlannerTestCommandGroup. */
+  DriveSubsystem m_driveSubsystem;
+
+  public AutonomousPathPlannerTestCommandGroup(DriveSubsystem m_driveSubsystem) {
+    this.m_driveSubsystem = m_driveSubsystem;
+    addRequirements(m_driveSubsystem);
+
+    PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
+    PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
+
+    ProfiledPIDController thetaController = new ProfiledPIDController(AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+    
+    PathPlannerTrajectory testPath = PathPlanner.loadPath("New Path", 5, 5);
+
+    PPSwerveControllerCommand command = new PPSwerveControllerCommand(
+        testPath,
+        this.m_driveSubsystem::getPose, 
+        this.m_driveSubsystem.m_kinematics, 
+        xController, 
+        yController, 
+        thetaController, 
+        this.m_driveSubsystem::setModuleStates, 
+        this.m_driveSubsystem
+    );
+
+    addCommands(
+      new InstantCommand(() -> this.m_driveSubsystem.zeroGyroscope()),
+      command,
+      new InstantCommand(() -> this.m_driveSubsystem.stopModules())
+    );
+  }
+}
