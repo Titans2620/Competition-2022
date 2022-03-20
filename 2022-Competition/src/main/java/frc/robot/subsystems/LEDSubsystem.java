27f29e5813c737e5296624d31c69b04eecc34149
FlintWeststeel <59623576+FlintWeststeel@users.noreply.github.com>
2022-03-20 17:13:11 -0400

    Some LED Code

--- 2022-Competition/src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ 2022-Competition/src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -14,18 +14,47 @@ public class LEDSubsystem extends SubsystemBase {
   AddressableLED led;
   AddressableLEDBuffer ledBuffer;
   Timer timer;
+  int blinkLength, blinkCount;
+  boolean blinkStateOn;
   /** Creates a new LEDSubsystem. */
   public LEDSubsystem() {
     led = new AddressableLED(Constants.LED);
-    ledBuffer = new AddressableLEDBuffer(1);
+    ledBuffer = new AddressableLEDBuffer(32);
     led.setData(ledBuffer);
     led.setLength(Constants.LEDLENGTH);
     timer.start();
+    led.start();
+    blinkLength = 3;
+    blinkCount = 0;
+    blinkStateOn = false;
   }
 
-  public void setColor(Color color){
-    ledBuffer.setLED(1, color);
-    led.start();
+  public void setSolidColor(int red, int green, int blue){
+    for(int index = 0; index < 32; index++){
+      ledBuffer.setRGB(index, red, green, blue);
+    }
+  }
+
+  public void setBlink(int red, int green, int blue){
+    if(blinkCount > blinkLength){
+      if(blinkStateOn){
+        blinkStateOn = false;
+      }
+      else{
+        blinkStateOn = true;
+      }
+      blinkCount = 0;
+    }
+    if(blinkStateOn){
+      for(int index = 0; index < 32; index++){
+        ledBuffer.setRGB(index, red, green, blue);
+      }
+    }
+    else{
+      for(int index = 0; index < 32; index++){
+        ledBuffer.setRGB(index, 0, 0, 0);
+      }
+    }
   }
 
   @Override
